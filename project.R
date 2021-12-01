0d1925cc1736548f5dc3c39aeb5e326938319409
Paula <paula.sorolla@gmail.com>
2021-12-01 16:26:51 +0100

    Added data

---
+++ project.R
@@ -0,0 +1,6 @@
+# Read the data
+calendar <- readRDS(file="data/calendar.Rda")
+train_eval <- readRDS(file="data/sales_train_evaluation.Rda")
+train_val <- readRDS(file="data/sales_train_validation.Rda")
+sell_prices <- readRDS(file="data/sell_prices.Rda")
+
